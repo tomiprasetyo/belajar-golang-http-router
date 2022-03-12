e89971284715285bf4b94e0fb21b8568ad6dba83
tomiprasetyo <tomi.prasetyo95@gmail.com>
2022-03-12 22:12:40 +0700

    7. create and test not allowed handler

---
+++ tests/not_allowed_test.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotAllowed(t *testing.T) {
+	router := httprouter.New()
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Not Allowed")
+	})
+
+	router.POST("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "POST")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Not Allowed", string(body))
+}
