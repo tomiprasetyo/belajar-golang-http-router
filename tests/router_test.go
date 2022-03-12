eb11b3adda8602894668f6bc44db8b195deef290
tomiprasetyo <tomi.prasetyo95@gmail.com>
2022-03-12 18:54:53 +0700

    1. create and test router handle

---
+++ tests/router_test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "GET")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	bytes, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "GET", string(bytes))
+}
