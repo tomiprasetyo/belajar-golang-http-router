60a159b6583d45a2499c04485f6fd4a0d9a1237a
tomiprasetyo <tomi.prasetyo95@gmail.com>
2022-03-12 21:29:13 +0700

    4. create and test serve file handler

---
+++ tests/serve_file_test.go
@@ -0,0 +1,31 @@
+package tests
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/files/test.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Test HttpRouter", string(body))
+}
