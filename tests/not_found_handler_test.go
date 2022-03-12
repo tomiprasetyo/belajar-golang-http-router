d8c5a2d9f4c1ee17cbee05eb67251e92d2e170f9
tomiprasetyo <tomi.prasetyo95@gmail.com>
2022-03-12 22:01:04 +0700

    6. create and test not found handler

---
+++ tests/not_found_handler_test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotFoundHandler(t *testing.T) {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Page not found")
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/products/5/items/10", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Page not found", string(body))
+}
